40f42becf6934d9a76c06f01637d2cfa766b87c5
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2021-11-18 04:21:52 +0100

    new unit testing system & clean-up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,18 @@
+# 0.8.0
+
+- New unit testing system
+- Re-designed playground
+
 # 0.7.0
 
-SVG moved inside components
+- SVG moved inside components
+- refactoring
+- Minor fixes
 
 # 0.6.0
 
-Clean up and optimizations
+- Clean up and optimizations
 
 # 0.5.0
 
-Modular-ui library setup
+- Modular-ui library setup
--- tests/core/main.test.tsx
+++ tests/core/main.test.tsx
@@ -1,31 +1,4 @@
-import {
-  APP_NAME,
-  CATEGORIES,
-  ROOT_DIR,
-  TEST_SUITES_PATH,
-} from "./config/test.config.json";
+import { APP_NAME, TESTS_PATH } from "./config/test.config.json";
+import { executeTests } from "./utils/helpers";
 
-/** Helpers */
-import { describeComponent, describeSection } from "./utils/helpers";
-
-describe(`\n                       ## ${APP_NAME} - Unit tests - v.0.5 ##`, () => {
-  Object.keys(CATEGORIES).map((category) =>
-    describeSection(
-      category.charAt(0).toUpperCase() + category.slice(1),
-      () => {
-        CATEGORIES[category].map((element) => {
-          describeComponent(element, () => {
-            try {
-              require(`${ROOT_DIR}${TEST_SUITES_PATH}${category}/${element}.testsuite.tsx`);
-            } catch(error) {
-              console.log(error)
-              console.log(
-                `${ROOT_DIR}${TEST_SUITES_PATH}${category}/${element}.testsuite not found`
-              );
-            }
-          });
-        });
-      }
-    )
-  );
-});
+executeTests(APP_NAME, TESTS_PATH);
