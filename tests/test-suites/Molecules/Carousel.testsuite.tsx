ac2724b25590c9365172d2ca1adb276c5ddf507f
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2021-12-27 05:45:56 +0100

    Carousel test fix

--- tests/test-suites/Molecules/Carousel.testsuite.tsx
+++ tests/test-suites/Molecules/Carousel.testsuite.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { mount, shallow } from "enzyme";
 import { Carousel } from "../../../src";
+import Sinon from "sinon";
+
+const onChangeStub = Sinon.stub();
 
 test("rendered without errors", () => {
-  let wrapper = mount(<Carousel elements={["elem1", "elem2", "elem3"]} />);
-  expect(wrapper);
+  let wrapper = mount(
+    <Carousel elements={["elem1", "elem2", "elem3"]} onChange={onChangeStub} />
+  );
   wrapper.find('div[data-id="dot_1"] button').simulate("click");
   wrapper.find('div[data-id="dot_0"] button').simulate("click");
   wrapper.find('div[data-id="right_arrow"] button').simulate("click");
   wrapper.find('div[data-id="left_arrow"] button').simulate("click");
-  expect(wrapper);
+  expect(onChangeStub).toBeCalled;
 });
 
 test("rendered without errors - no parameters", () => {
