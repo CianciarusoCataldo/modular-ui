36a3f75561dd710f5cf7b8ad7edc3e0b22efbe47
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2022-02-14 03:56:44 +0100

    updates

--- playground/src/contents/footer.tsx
+++ playground/src/contents/footer.tsx
@@ -1,33 +1,17 @@
 const FooterContent = () => {
   return (
-    <div className="flex flex-col items-center my-2">
-      <div className="flex flex-row mb-1">
+    <div className="flex flex-col items-center h-full w-full">
+      <div className="flex flex-row m-auto">
         <img
           alt=""
           className="p-1"
           src="https://img.shields.io/npm/l/@cianciarusocataldo/modular-ui?label=License"
           height="25"
         />
-
         <img
           alt=""
           className="p-1"
           src="https://img.shields.io/npm/v/@cianciarusocataldo/modular-ui?color=blue&label=Latest%20version"
-          height="25"
-        />
-      </div>
-
-      <div className="flex flex-row">
-        <img
-          alt=""
-          className="p-1"
-          src="https://img.shields.io/codecov/c/github/CianciarusoCataldo/modular-ui?label=Codecov%20coverage"
-          height="25"
-        />
-        <img
-          alt=""
-          className="p-1"
-          src="https://github.com/CianciarusoCataldo/modular-ui/actions/workflows/modular-ui.yml/badge.svg?branch=main)](https://github.com/CianciarusoCataldo/modular-ui/actions/workflows/modular-ui.yml"
           height="25"
         />
       </div>
