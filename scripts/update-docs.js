020369e58711878de5f085b5914517da5e71c177
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2022-02-28 05:13:32 +0100

    fix

--- scripts/update-docs.js
+++ scripts/update-docs.js
@@ -5,33 +5,35 @@ var buffer = fs.readFileSync("./CHANGELOG.md");
 var fileContent = buffer.toString();
 
 var splitted = fileContent.split("## Versions");
+var versionAlreadyPresent = splitted[1]
+  .split("[")[1]
+  .startsWith(PKG.version.toString());
 
-var resVersions = !splitted[1].split("[")[1].startsWith(PKG.version.toString())
-  ? `-   [${PKG.version}](#${PKG.version})
-${splitted[1]}`
-  : splitted[1];
+var resVersions = "";
+
+if (!versionAlreadyPresent) {
+  resVersions =
+    "-   [" + PKG.version + "](#" + PKG.version + ")\n" + splitted[1];
+} else {
+  resVersions = splitted[1];
+}
 
 var splittedChanges = resVersions.split("## Changes");
-var changes = splittedChanges[1].split("### ");
 
-var resChanges = !changes[1].startsWith(PKG.version.toString())
-  ? `
-<br>
+var resChanges = "";
 
-### ${PKG.version}
--   
-
-<br>
-${splittedChanges[1]}`
-  : splitted[1];
+if (!versionAlreadyPresent) {
+  resChanges =
+    "\n<br>\n\n### " + PKG.version + "-test\n-   \n\n" + splittedChanges[1];
+} else {
+  resChanges = splittedChanges[1];
+}
 
 fs.writeFileSync(
   "./CHANGELOG.md",
-  `${splitted[0]}
-## Versions
-${resVersions}
-
-## Changes
-
-${resChanges}`
+  splitted[0] +
+    "## Versions" +
+    splittedChanges[0] +
+    "## Changes" +
+    resChanges
 );
